220393d93f51889a4b29330bd632925ec60ed959
OusB2 <ousmanebamba966@gmail.com>
2026-08-21 10:16:51 +0000

    implémentation de l'écran Quadrant Compose - Correction des versions Gradle (coreKtx, lifecycleRuntimeKtx, compileSdk/targetSdk 36) - Séparation MainActivity.kt (point d'entrée) et ComposeQuadrant.kt (composables) - 4 quadrants avec Row/Column + weight(1f), couleurs et textes conformes au cours

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.afrocoiff.composequadrant"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.afrocoiff.composequadrant"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
